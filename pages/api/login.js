e5344cee2745eb417ab677b7a40d4a07f4594b94
Yuchen Jiao <jiaoyuchen0418@gmail.com>
2023-05-06 00:05:21 -0700

    fix bug when doing api authorization

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,55 +1,48 @@
 import { MongoClient } from "mongodb";
 import { genMongoToken } from "@/helpers/genMongoToken";
-import { withSessionRoute } from "pages/lib/config/withSession";
-import { authenticate } from "@/helpers/authenticate";
+import { withSessionRoute } from "@/lib/config/withSession";
 
-// export default handler;
 export default withSessionRoute(handler);
 
 async function handler(req, res) {
-  const auth = authenticate(req);
-  if (!auth.isValid) {
-    res.status(401).json("Unauthorized");
-  } else {
-    if (req.method === "GET") {
-      const token = genMongoToken("users");
-      try {
-        const client = await MongoClient.connect(token);
-        const db = client.db();
-        const collection = db.collection("users");
-        const { username, password } = req.query;
-        const result = await collection.findOne({ username: username });
-        let isCorrectUser = false;
-        if (result) {
-          const bcrypt = require("bcrypt");
-          const parsed = {
-            id: result._id.toString(),
-            username: result.username,
-            hash: result.password,
-          };
-          isCorrectUser = bcrypt.compareSync(password, parsed.hash);
-        }
-        client.close();
-        if (isCorrectUser) {
-          req.session.user = {
-            id: result._id.toString(),
-            username: result.username,
-            avatar: result.avatar ? result.avatar : "/img/Avatar/jean.png",
-          };
-          await req.session.save();
-          res.status(200).json({
-            msg: "Login succeed",
-          });
-        } else {
-          res.status(401).json({
-            msg: "Invalid credential",
-          });
-        }
-      } catch (err) {
-        res.status(500).json({
-          err,
+  if (req.method === "GET") {
+    const token = genMongoToken("users");
+    try {
+      const client = await MongoClient.connect(token);
+      const db = client.db();
+      const collection = db.collection("users");
+      const { username, password } = req.query;
+      const result = await collection.findOne({ username: username });
+      let isCorrectUser = false;
+      if (result) {
+        const bcrypt = require("bcrypt");
+        const parsed = {
+          id: result._id.toString(),
+          username: result.username,
+          hash: result.password,
+        };
+        isCorrectUser = bcrypt.compareSync(password, parsed.hash);
+      }
+      client.close();
+      if (isCorrectUser) {
+        req.session.user = {
+          id: result._id.toString(),
+          username: result.username,
+          avatar: result.avatar ? result.avatar : "/img/Avatar/jean.png",
+        };
+        await req.session.save();
+        res.status(200).json({
+          msg: "Login succeed",
+        });
+      } else {
+        res.status(401).json({
+          msg: "Invalid credential",
         });
       }
+    } catch (err) {
+      res.status(500).json({
+        err,
+      });
     }
   }
 }
